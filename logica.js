00d3667b3504a92460d28d9a57380ed0ed205145
ConstantinoJV <ingcontantin78@gmail.com>
2023-08-30 21:35:00 -0500

    segunda entrega

--- logica.js
+++ logica.js
@@ -4,40 +4,83 @@ let nombre = prompt ("Bienvenido, cual desea que sea su Nombre de usuario?");
 
 
 
-function nivel() {
-    let n  = prompt (nombre + ", del 1 al 5 a que nivel deseas modificar tu coche ?");
-    return n;
+class Nivel{
+    constructor(id,descrip,precio){
+        this.id = id
+        this.descrip = descrip
+        this.precio = precio
+    }
+    descripcion(){
+        return "Nivel: "+this.id+"=>  "+this.descrip+", con un costo de "+this.precio+"$\n" 
+    }
 }
-let a = nivel();
+
+class ControlNivel{
+    constructor(){
+        this.listaNiveles = []
+    }
+    agregar(nivel){
+        this.listaNiveles.push(nivel)
+    }
+    mostrar(){
+        let acumuladora = "Listado de niveles\n\n "
+        this.listaNiveles.forEach(nivel => {
+            acumuladora = acumuladora + nivel.descripcion()
+        })
+        return acumuladora
+    }
+}
+
+const n1 = new Nivel(1,"Optimizar tu sistema de admision, escape, y frenos",5000)
+const n2 = new Nivel(2,"Optimizacion del sistema de suspension e inyeccion",9000)
+const n3 = new Nivel(3,"Mejora de la rigides del compacto, programacion de la ecu",12000)
+const n4 = new Nivel(4,"Implementacion de sistema de induccion forzada y mejora de la ignicion",18000)
+const n5 = new Nivel(5,"Performanse de todos los sistemas",20000)
+
+const controlN = new ControlNivel
+
+controlN.agregar(n1)
+controlN.agregar(n2)
+controlN.agregar(n3)
+controlN.agregar(n4)
+controlN.agregar(n5)
+
+alert (controlN.mostrar())
 
 
 
-while (a<1 || a>5){
-    alert ("opcion invalida, intente denuevo");
+function nivel() {
+    return  prompt (nombre + ", del 1 al 5 selecciona que modificacion quieres para tu vehiculo ?");   
+}
+let id = nivel();
+
+while (id<1 || id>5){
+    alert ("modificacion invalida, intente denuevo");
     nivel();
     break;
 }
 
-function seleccion(a){
 
-    if (a==1){
-        alert ( "Puedes optimizar tu sistema de admision, escape, y frenos con un costo de 5000$");
+function seleccion(id){
+
+    if (id==1){
+        alert ( "Seleccionastes optimizar tu sistema de admision, escape, y frenos con un costo de 5000$");
     }
-    else if(a==2){
-        alert ("Optimizacion del sistema de suspension, inyeccion con un costo de 9000$");
+    else if(id==2){
+        alert ("Seleccionastes optimizacion del sistema de suspension e inyeccion con un costo de 9000$");
     }
-    else if(a==3){
-        alert ("Mejora de la rigides del compacto, programacion de la ecu, con un costo de 12000$");
+    else if(id==3){
+        alert ("Seleccionastes mejora de la rigides del compacto, programacion de la ecu, con un costo de 12000$");
     }
-    else if(a==4){
-        alert ("implementacion de sistema de induccion forzada y mejora de la ignicion con un costo de 18000$");
+    else if(id==4){
+        alert ("Seleccionastes implementacion de sistema de induccion forzada y mejora de la ignicion con un costo de 18000$");
     }
-    else if(a==5){
-        alert ("Performanse de todos los sistemas con un costo base de 20000$");
+    else if(id==5){
+        alert ("Seleccionastes performanse de todos los sistemas con un costo base de 20000$");
     }
 }
 
-seleccion(a);
+seleccion(id);
 
 
 
